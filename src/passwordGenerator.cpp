ccedd8b9ed97fb7b9d3b567a0e8a3df38ad8eec3
Isabella <isabella.salim3@gmail.com>
2026-07-01 00:01:36 +0300

    Cleaned and validated input in passwordGenerator.cpp

--- src/passwordGenerator.cpp
+++ src/passwordGenerator.cpp
@@ -3,55 +3,24 @@
 #include <iostream>
 #include <random> 
 #include "../include/utility.h"
-//Password Strengths:
-//1. easy -> num or letters
-//2. medium -> num + letters
-//3. strong ->  num + letters + special chars
-
-
-//MISSING:
-// [ ] Input validation 
-    // invalid menu selections
-    //  non numeric input
-    // division by zero
-    // empty inputs
-// [ ] Error Handling + Messages
-// [ ] Comments to explain parts of code
-// [ ] Function design - each function should have a single responsibility
-// [ ] Code organization - organize code logically with related functions grouped together
-
-
 using namespace std;
-void easyPasswordGenerator();
-void mediumPasswordGenerator();
-void strongPasswordGenerator();
-void menu(); //delete?
 
 int passwordGenerator(){
-    
     cout << "\n\n";
     cout << "=================" << endl;
     cout << "Password Generator"<< endl;
     cout << "=================" << endl;
 
     int input = 0;
+    const int min = 1;
+    const int max = 3;
 
     cout << "1. Easy - contains numbers or letters" <<endl;
     cout << "2. Medium - contains both numbers and letters" <<endl;
     cout << "3. Strong - contains numbers, letters and special characters" <<endl;
     cout << "Choose your password strength (1 - 3): " << endl;
 
-
-    //validates input
-    while(!(cin >> input) || input > 3 || input < 1 ){ 
-        cout << "Invalid input. Please enter a valid number (1 - 3): "<< endl;
-        cin.clear(); //resets the stream error state flags
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discards all invalid characters remaining in the input buffer
-    }
-
-    //error handling to be done ******
-
-    
+    input = getValidatedInput(min,max);
 
     switch (input){
         case 1:
@@ -66,7 +35,6 @@ int passwordGenerator(){
         default:
             break;
     }
-
     return 0;
 }
 
@@ -83,7 +51,6 @@ void easyPasswordGenerator(){
     char letter;
     string result;
     int num = 0;
-   
     cout << '\n';
     
     for(int i = 0; i < length; i++){
@@ -104,11 +71,8 @@ void easyPasswordGenerator(){
 }
 
 void mediumPasswordGenerator(){
-    // num + letters
-
     srand(time(0));
     const int length = 6; 
-
     int randomNum = 0;
     char letter;
     char digit;
@@ -124,23 +88,18 @@ void mediumPasswordGenerator(){
         result += letter;
 
     }
-
     cout << "== Medium password (numbers + letters): ";
     cout << result <<endl;
 
 }
 
 void strongPasswordGenerator(){
-    //num + letters + special chars
-
     srand(time(0));
     const int length = 6; 
-
     int randomNum = 0;
     char letter;
     char digit;
     string result; 
-
     string specialChars[6] = {"'", ",", "@", "#", ".", "$"};
 
     for(int i = 0; i < length; i++){
@@ -151,30 +110,13 @@ void strongPasswordGenerator(){
         letter = 'a' + randomNum;
         result += letter;
 
-        //first generate a randomNum
-        //then based on the number, the index of specialChars is chosen 
-        // then added to result
-
         randomNum = rand() % 6;
         for(int i = 0; i < size(specialChars); i++){
             if(randomNum == i){
                 result += specialChars[i];
+            }
         }
     }
-    
-
-        
-    }
-    
-
     cout << "== Strong password (numbers + letters + special chars): ";
     cout << result <<endl;
-
-
-}
-
-
-void menu(){
-
-
 }
