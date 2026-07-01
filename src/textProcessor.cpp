48ee7acf716cf04cfdd6bc49cb83bb1132d251fe
Isabella <isabella.salim3@gmail.com>
2026-07-01 21:12:10 +0300

    Cleaned and validated inputs

--- src/textProcessor.cpp
+++ src/textProcessor.cpp
@@ -1,20 +1,6 @@
 #include <iostream>
 #include "../include/utility.h"
 using namespace std;
-//MISSING:
-// [ ] Input validation 
-    // invalid menu selections
-    //  non numeric input
-    // division by zero
-    // empty inputs
-// [ ] Error Handling + Messages
-// [ ] Comments to explain parts of code
-// [ ] Function design - each function should have a single responsibility
-// [ ] Code organization - organize code logically with related functions grouped together
-
-
-void encrypter();
-void decrypter();
 
 int textProcessor(){
 
@@ -24,20 +10,14 @@ int textProcessor(){
     cout << "==============================" << endl;
 
     int input = 0;
+    const int min = 1;
+    const int max = 2;
 
     cout << "1. Encrypter" <<endl;
     cout << "2. Decrypter" <<endl;
     cout << "Choose an option (1 - 2): " << endl;
 
-    input = getValidatedInput(1,2);
-
-
-  /*  while(!(cin >> input) || input > 2 || input < 1 ){ 
-        cout << "Invalid input. Please enter a valid number (1 - 3): "<< endl;
-        cin.clear(); //resets the stream error state flags
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discards all invalid characters remaining in the input buffer
-    }*/
-
+    input = getValidatedInput(min,max);
 
     switch(input){
         case 1:
@@ -53,7 +33,7 @@ int textProcessor(){
 }
 
 
-void encrypter(){ // issue with multiple words
+void encrypter(){
     string plainText;
     int shift = 0; // controls how far each letter is moved in the alphabet
     int index = 0;
@@ -62,15 +42,13 @@ void encrypter(){ // issue with multiple words
     string cipherText;
 
     cout << "Input your plaintext message: " << endl;
-    cin >> plainText; //string validation
+    plainText = validateString();
 
     cout << "Input the number of shifts: " <<endl;
-    cin >> shift;
+    shift = validateInt();
 
     for(char letter : plainText){
-
         if(isalpha(letter)){
-            //cout << m << endl;
             index = letter - 'a';
             newIndex = (index + shift)  % 26;
             newChar = 'a' + newIndex;
@@ -83,7 +61,6 @@ void encrypter(){ // issue with multiple words
 
 }
 
-
 void decrypter(){
     string plainText;
     int shift = 0; // controls how far each letter is moved in the alphabet
@@ -93,10 +70,10 @@ void decrypter(){
     string cipherText;
 
     cout << "Input your cipher text: " << endl;
-    cin >> cipherText;
-
+    cipherText = validateString();
+    
     cout << "Input the number of shifts: " <<endl;
-    cin >> shift;
+    shift = validateInt();
 
     for(char letter : cipherText){
         if(isalpha(letter)){
