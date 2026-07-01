556461d91befa058d8dfc17ea287acf6aef798dc
Isabella <isabella.salim3@gmail.com>
2026-07-01 22:03:20 +0300

    Added string and int validater

--- src/input_utils.cpp
+++ src/input_utils.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <limits>
+#include <cctype>
 #include "../include/input_utils.h"
 using namespace std;
+
 
 int getValidatedInput(int min, int max){
     int value = 0;
@@ -11,4 +13,36 @@ int getValidatedInput(int min, int max){
         cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discards all invalid characters remaining in the input buffer
     }
     return value;
+}
+
+int validateInt(){
+    int num;
+    while(!(cin >> num)){
+        cout << "Invalid input. Please enter a number  " << endl;
+        cin.clear(); //resets the stream error state flags
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discards all invalid characters remaining in the input buffer
+    }
+    return num;
+}
+
+string validateString(){
+    string input;
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); //discards all invalid characters remaining in the input buffer
+    while(true){
+        getline(cin, input);
+        //bool result = isLettersOnly(input);
+       /* if(input.empty()){
+            cout << "Invalid input. Please enter a non-empty string: " << endl;
+        } else if(!result){
+            cout << "Invalid input. Please enter a string consisting of letters only: " <<endl;
+        } else{
+            return input;
+        }*/
+
+        if(!input.empty()){
+            return input; 
+        }
+        cout << "Invalid input. Please enter a non-empty string: " << endl;
+    }
 }
